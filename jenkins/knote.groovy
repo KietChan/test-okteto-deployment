21860087fed0f7f7e8192e3a2549942ce3c25b95
Kiet.Chan <kietlyc@nashtechglobal.com>
2021-04-07 15:59:22 +0700

    WIP-02

---
+++ jenkins/knote.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                // Get some code from a GitHub repository
+                git 'https://github.com/KietChan/test-okteto-deployment'
+            }
+            // post {
+            //     // If Maven was able to run the tests, even if some of the test
+            //     // failed, record the test results and archive the jar file.
+            //     success {
+            //         junit '**/target/surefire-reports/TEST-*.xml'
+            //         archiveArtifacts 'target/*.jar'
+            //     }
+            // }
+        }
+    }
+}
