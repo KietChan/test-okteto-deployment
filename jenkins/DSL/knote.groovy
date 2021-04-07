c3903f56a01040e9f0c978adfdf12f78942a6125
Kiet.Chan <kietlyc@nashtechglobal.com>
2021-04-07 21:01:05 +0700

    a

--- jenkins/DSL/knote.groovy
+++ jenkins/DSL/knote.groovy
@@ -13,16 +13,16 @@ pipeline {
         }
         stage('Docker Build') {
             steps {
-                sh "docker build --platform linux/amd64 -f app/Dockerfile -t lazyk9/knote:latest -t lazyk9/knote:$BUILD_NUMBER ./app"
+                sh "docker build --platform linux/amd64 -f app/Dockerfile -t lazyk9/knote:latest -t lazyk9/knote:0.0.$BUILD_NUMBER ./app"
             }
         }
         stage('Docker Push') {
             steps {
                 sh "docker image push lazyk9/knote:latest"
-                sh "docker image push lazyk9/knote:$BUILD_NUMBER"
+                sh "docker image push lazyk9/knote:0.0.$BUILD_NUMBER"
             }
         }
-        stage('Signal OKteto') {
+        stage('Deploy') {
             steps {
                 sh "okteto pipeline deploy --repository=https://github.com/KietChan/test-okteto-deployment"
             }
