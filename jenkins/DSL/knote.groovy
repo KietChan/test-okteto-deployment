2bd51ded1cc041fd68fac73b485012ba8c34f34e
Kiet.Chan <kietlyc@nashtechglobal.com>
2021-04-08 09:42:48 +0700

    s

--- jenkins/DSL/knote.groovy
+++ jenkins/DSL/knote.groovy
@@ -6,20 +6,23 @@ pipeline {
                 git 'https://github.com/KietChan/test-okteto-deployment'
             }
         }
-        stage('NodeJS Build') {
+        stage('Build') {
             steps {
                 sh 'npm install app/'
             }
         }
-        stage('Docker Build') {
+        stage('Docker') {
             steps {
                 sh "docker build --platform linux/amd64 -f app/Dockerfile -t lazyk9/knote:latest -t lazyk9/knote:0.0.$BUILD_NUMBER ./app"
-            }
-        }
-        stage('Docker Push') {
-            steps {
                 sh "docker image push lazyk9/knote:latest"
                 sh "docker image push lazyk9/knote:0.0.$BUILD_NUMBER"
+            }
+        }
+        stage('Git') {
+            steps {
+                sh 'sed "s/__build_version__/$BUILD_NUMBER/g" k8s-dev/knote.yaml > k8s/knote.yaml'
+                sh 'sed "s/__build_version__/$BUILD_NUMBER/g" k8s-dev/minio.yaml > k8s/minio.yaml'
+                sh 'sed "s/__build_version__/$BUILD_NUMBER/g" k8s-dev/mongo.yaml > k8s/mongo.yaml'
             }
         }
         stage('Deploy') {
